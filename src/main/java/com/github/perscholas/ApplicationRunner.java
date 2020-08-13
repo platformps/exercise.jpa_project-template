ae20fad113e347dc91390b8041f25943a546dd8d
git-leon <xleonhunter@gmail.com>
2020-08-13 14:05:55 -0400

    update

---
+++ src/main/java/com/github/perscholas/ApplicationRunner.java
@@ -0,0 +1,12 @@
+package com.github.perscholas;
+
+import com.github.perscholas.utils.IOConsole;
+
+public class ApplicationRunner implements Runnable {
+    private static final IOConsole console = new IOConsole();
+
+    @Override
+    public void run() {
+        // TODO - Define implementation here
+    }
+}
